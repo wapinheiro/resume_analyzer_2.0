104cdd6b277b9cee48d67fa23550fad020316cb4
Wagner <wapinheiro@gmail.com>
2026-09-17 17:49:11 -0600

    fix(alembic): resolve multiple heads by setting down_revision to 7969d5f0b142

--- backend/alembic/versions/a1b2c3d4e5f6_add_leaderboard_opt_in.py
+++ backend/alembic/versions/a1b2c3d4e5f6_add_leaderboard_opt_in.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, None] = '52b8cfddbe1a'
+down_revision: Union[str, None] = '7969d5f0b142'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
